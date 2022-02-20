27a8465f9f310d2015dcf5d6eeedbb482bbd2b55
yeliu <yeliu0123@gmail.com>
2022-02-20 12:53:44 +0000

    add code comment

--- raft/lib/appendentries.ex
+++ raft/lib/appendentries.ex
@@ -1,4 +1,4 @@
-# Alicia Law() and Ye Liu(yl10321)
+# Alicia Law(ajl115) and Ye Liu(yl10321)
 # distributed algorithms, n.dulay, 8 feb 2022
 # coursework, raft consensus, v2
 
@@ -7,32 +7,44 @@ defmodule AppendEntries do
 # s = server process state (c.f. this/self)
 
   def send_entries_to_followers(leader, followerP) do
+  # Called when a leader receives client request or append entries timeout
+  # Inputs:
+  #   - leader              : server who receives client request
+  #   - followerP           : follower's process id
+
+    # (i) If follower's next_index < leader's log length + 1, append new entries
     if leader.next_index[followerP] < (Log.last_index(leader) + 1) do
 
-      # #IO.puts "CURRENT LOG LENGTH: #{map_size(leader.log)}"
-
-      followerPrevIndex = leader.next_index[followerP] - 1                              # follower's the next_index (where to start the entry)
+      followerPrevIndex = leader.next_index[followerP] - 1                              # follower's next_index - 1 (where to start the entry)
       entries = Log.get_entries(leader, (followerPrevIndex+1)..Log.last_index(leader))  # entries
-      prevTerm = Log.term_at(leader, followerPrevIndex)
+      prevTerm = Log.term_at(leader, followerPrevIndex)                                 # term which followerPrevIndex is at
 
-      # #IO.puts("Line 24 - APPEND_ENTIRES_REQ contains following: followerPrevIndex: #{followerPrevIndex}, entries: #{inspect entries}, prevTerm: #{prevTerm} ")
+      # send append entries request to follower
       Timer.restart_append_entries_timer(leader,followerP)
       send followerP, { :APPEND_ENTRIES_REQUEST, leader.curr_term, followerPrevIndex, prevTerm, entries, leader.commit_index}
-      # IO.inspect(leader, label: "leader #{leader.server_num} after sending entries to #{inspect(followerP)}")
       leader
-    else # if follower is already up-to-date, just send heartbeats
+
+    # (ii) If follower is already up-to-date, leader sends dummy heartbeats to follower
+    else
       Timer.restart_append_entries_timer(leader,followerP)
       send followerP, { :APPEND_ENTRIES_REQUEST, leader.curr_term, leader.commit_index }
-      # #IO.puts("Followers are up-to-date. Sending heartbeats.")
       leader
     end
     leader
   end
 
   def receive_append_entries_request(s, leaderTerm, prevIndex, prevTerm, leaderEntries, commitIndex) do
+  # Called when a server receives leader's append entries request
+  # Inputs:
+  #   - s                   : leader who sends append entries request
+  #   - leaderTerm          : leader's current term
+  #   - prevIndex           : follower's next_index - 1
+  #   - prevTerm            : term of follower's prevIndex
+  #   - leaderEntries       : entries to append to follower's log
+  #   - commitIndex         : leader's commit_index
+
     # (i) If I am a candidate/ leader but received an append entries request from someone with a larger term, stepdown:
     s = if s.role != :FOLLOWER && s.curr_term < leaderTerm do
-      #IO.puts("Server #{s.server_num} stepdown as received aeReq from another leader of larger term")
       s = Vote.stepdown(s, leaderTerm)
     else
       s
@@ -40,11 +52,10 @@ defmodule AppendEntries do
 
     # (ii) If my current term is larger than the leader's, reject leader
     if s.curr_term > leaderTerm do
-      # #IO.puts("Server #{s.server_num} rejecting aeReq from leader bec have a larger term")
       send s.leaderP, {:APPEND_ENTRIES_REPLY, s.selfP, s.curr_term, false, nil}
     end # if
 
-    # (iii) If my current term == leader's
+    # (iii) If my current term == leader's term
     # Check if can successfully append: i.e. if our prev index and terms match too
     success = (s.curr_term == leaderTerm) && (prevIndex == 0 || (prevIndex <= Log.last_index(s) && prevTerm == Log.term_at(s, prevIndex)))
 
@@ -64,8 +75,14 @@ defmodule AppendEntries do
 
 
   def storeEntries(s, prevIndex, entries, commitIndex) do
+  # Called when a server receives leader's append entries request
+  # Inputs:
+  #   - s                   : leader who sends append entries request
+  #   - prevIndex           : follower's next_index - 1
+  #   - entries             : entries to append to follower's log
+  #   - commitIndex         : leader's commit_index
 
-    # find the point in the entry to start appending - min of breakpoint
+    # (i) Find the point in the entry to start appending
     breakPointList = for {index, v} <- entries do
       if Log.last_index(s) >= index do
         if s.log[index].term != v.term do
@@ -80,52 +97,56 @@ defmodule AppendEntries do
 
     breakPoint = Enum.min(breakPointList)                 # the index where the server's log and entries start to diverge
 
+    # (ii) Delete extraneous entries
     entries = if breakPoint != nil do
-      Map.drop(entries, Enum.to_list(0..(breakPoint-1)))  # clear the entries before breakPoint
+      Map.drop(entries, Enum.to_list(0..(breakPoint-1)))  # delete the entries before breakPoint (which have been appended to follower's log)
     else
       %{}
     end
 
-    # delete logs if prevIndex is old
     s = if breakPoint != nil && breakPoint < Log.last_index(s) do
       s = s|>
-      Log.delete_entries_from(breakPoint)    # Delete entries from the point where diverge with leader
-      # IO.inspect(s.log, label: "Server #{s.server_num}'s logs after delete_entries_from")
+      Log.delete_entries_from(breakPoint)               # delete entries from the point where diverge with leader
       s
     else
       s
     end
 
-    # merge logs from breakPoint onwards
+    # (iii) Merge logs from breakPoint onwards
     s = if entries != %{} do
-      s = Log.merge_entries(s, entries)                 # Append leader's entries
-      s = State.commit_index(s, Log.last_index(s))      # Update commit index (for logs)
-      # IO.inspect(s.log, label: "Server #{s.server_num}'s logs updated")
+      s = Log.merge_entries(s, entries)                 # append missing entries
+      s = State.commit_index(s, Log.last_index(s))      # update commit index (for logs)
       s
     else
-      # #IO.puts("already updated. Server #{s.server_num}'s logs stay the same")
       s
     end
     s
   end
 
   def receive_append_entries_reply_from_follower(s, followerP, followerTerm, success, followerLastIndex) do
-   # followerTerm here is definitely <= s.curr_term and s is a leader
+  # Called when a leader receives follower's reply for appending entries request
+  # Inputs:
+  #   - s                   : leader who sends append entries request
+  #   - followerP           : follower's process id
+  #   - followerTerm        : follower's current term
+  #   - success             : check append entries consistency
+  #   - followerLastIndex   : follower's log length
 
-   # Update leader's next_index tracker with follower
+  # followerTerm here is definitely <= leader.curr_term
+
+   # Update leader's next_index tracker with follower when consistency check success
    s = if success do
       s = State.next_index(s, followerP, followerLastIndex+1)
-      # #IO.puts("SUCCESSFUL aeReply - ae.ex Line 97. ")
       s
+
+  # Decrement follower's next_index when consistency check fails
    else
       s = State.next_index(s, followerP, max(s.next_index[followerP]-1, 1))
       send_entries_to_followers(s, followerP)
-      # #IO.puts("FAILED aeReply - ae.ex Line 97. ")
       s
    end
 
-   # Check if majority has committed to log
-
+   # Check if majority of servers have committed request to log
    counter = for i <- (s.last_applied+1)..Log.last_index(s) // 1,
     into: Map.new
     do
@@ -139,15 +160,13 @@ defmodule AppendEntries do
    end
 
    for {index, c} <- counter do
-    # #IO.puts "checking count #{c} and majority #{s.majority}"
+    # If leader received majority reply from followers, notify the local database to store the request
     if c >= s.majority do
-      # #IO.puts "Leader #{s.server_num} got majority reply, sending request #{index} to database"
-      send s.databaseP, {:DB_REQUEST, Log.request_at(s, index), index} # send the database the request
+      send s.databaseP, {:DB_REQUEST, Log.request_at(s, index), index}
     else
-      # #IO.puts "do not commit request #{index}"
+      # IO.puts "do not commit request #{index}"
     end
    end
-
    s
   end
 
--- raft/lib/clientreq.ex
+++ raft/lib/clientreq.ex
@@ -1,4 +1,4 @@
-
+# Alicia Law(ajl115) and Ye Liu(yl10321)
 # distributed algorithms, n.dulay, 8 feb 2022
 # coursework, raft consensus, v2
 
@@ -8,26 +8,30 @@ defmodule ClientReq do
 
 # Leader process client request, send entries to all followers
 def receive_request_from_client(leader, m) do
+  # Called when a client sends a :CLIENT_REQUEST to leader.
+  # Inputs:
+  #   - leader              : receipient of the client request
+  #   - m                   : client request
+
+  # (i) Check if cilent request has been processed by leader
   status = check_req_status(leader, m.cid)
 
+  # (ii) If client request has already been applied to database, just send :CLIENT_REPLY to client
   if status == :APPLIED_REQ do
     send m.clientP, { :CLIENT_REPLY, m.cid, m, leader.selfP, leader.server_num }
-    # #IO.puts("received applied request #{inspect m} from client, just reply.")
     leader
   end
 
-  # IO.inspect(leader.log, label: "before received request from client. Leader log: ")
+  # (ii) If client request has not been applied to database or appended to its log
   leader = if status == :NEW_REQ do
-    leader = Log.append_entry(leader, %{request: m, term: leader.curr_term})
+    leader = Log.append_entry(leader, %{request: m, term: leader.curr_term})    # append client request to leader's log
     leader = State.commit_index(leader, Log.last_index(leader))                 # update the commit index for in the logs
-    #IO.puts ("Leader log length: #{Log.last_index(leader)}")
-    # IO.inspect(leader.log, label: "line 22 Received request from client. Leader log:")
     leader
   else
     leader
   end
-  # IO.inspect(leader.log, label: "line 14 Leader log: ")
 
+  # (iii) If client request is a new request, send append entries request to other servers except itself
   for n <- leader.servers do
     if n != leader.selfP && status == :NEW_REQ do
       AppendEntries.send_entries_to_followers(leader, n)
@@ -36,47 +40,58 @@ def receive_request_from_client(leader, m) do
       leader
     end # if
   end # for
-
   leader
 end
 
 def receive_reply_from_db(leader, db_seqnum, client_request) do
+  # Called when a database sends a :CLIENT_REQUEST to leader.
+  # Inputs:
+  #   - leader              : receipient of the database reply
+  #   - db_seqnum           : index of request in database
+  #   - client_request      : client request
+
+  # (i) Update leader's last_applied value
   leader = leader |> State.last_applied(db_seqnum)
-  # #IO.puts "db_seqnum: #{db_seqnum}"
-  # #IO.puts "leader last_applied after receive_reply_from_db #{leader.last_applied}"
-  #IO.puts "Leader #{leader.server_num} send client_request #{inspect(client_request.cid)} #{inspect(client_request)} to client #{inspect client_request.clientP}"
+
+  # (ii) Leader send reply to client that server has applied the request
   send client_request.clientP, { :CLIENT_REPLY, client_request.cid, client_request, leader.selfP, leader.server_num }
 
+  # (iii) Leader broadcast to followers to commit the request to their local database
   for followerP <- leader.servers do
     send followerP, {:COMMIT_ENTRIES_REQUEST, db_seqnum}
   end
-
   leader
 end
 
 def check_req_status(leader, cid) do
+  # Check client request status in servers
+  # Inputs:
+  #   - leader              : receipient of the client request
+  #   - cid                 : unique id in client request
 
+  # Requests and cids that have been appended to log but not applied to database
   committedLog = Map.take(leader.log, Enum.to_list(1..leader.commit_index))
   committed_cid = for {k,v} <- committedLog, do: v.request.cid
 
+  # Requests and cids that have been applied to database
   appliedLog = Map.take(leader.log, Enum.to_list(1..leader.last_applied))
   applied_cid = for {k,v} <- appliedLog, do: v.request.cid
 
-  # #IO.puts("committed_cid: #{inspect(committed_cid)}, applied_cid: #{inspect(applied_cid)}")
   status = cond do
-    # If log is empty, definitely a new request
+    # If leader log is empty, it is a new request, return :NEW_REQ
     Log.last_index(leader) == 0 ->
       :NEW_REQ
 
+    # If cid is in applied_cid, it has already been applied to database, return :APPLIED_REQ
     Enum.find(applied_cid, nil, fn entry -> entry == cid end) != nil ->
       :APPLIED_REQ
 
+    # If cid is in committed_cid (but not in applied_cid), it has been appended to log, return :COMMITTED_REQ
     Enum.find(committed_cid, nil, fn entry -> entry == cid end) != nil ->
       :COMMITTED_REQ
 
     true ->
       :NEW_REQ
-
     end
 status # return
 end
